0d1da68b5aff7542aa068566e786dfebde490902
Julien Roth <julien@infraroth.com>
2021-09-29 18:23:24 +0200

    Added first files

---
+++ includes/_header.php
@@ -0,0 +1,17 @@
+<header>
+
+    <nav>
+        <div class = LogoNav></div>
+        <div class = MenuNav>
+            <ul>
+                <li>
+                <li>
+                <li>
+                <li>
+
+    
+            </ul>
+
+    </nav>
+
+</header>
