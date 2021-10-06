a9701c788759b63b4fc88b629b4867a12e84dd92
Julien Roth <julien@infraroth.com>
2021-10-06 17:30:18 +0200

    Updated css, includes and js

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,21 +1,22 @@
 <footer>
-    <ul class="navFooter">
-        <li>Acceuil</li>
-        <li>Articles
-        <li><span>watch dev is it?</span></li>
-        <li>Équipe</li>
-        <li>Contribuer</li>
-    </ul>
-
-    <div class="applications">
-        <!-- Facebook-->
-        <li a href="#"></li>
-        <!-- Instagram-->
-        <li a href="#"></li>
-        <!-- Twitter-->
-        <li a href="#"></li>
-        <!-- Linkedin-->
-        <li a href="#"></li>
+    <div class="footer-container">
+        <ul class="navFooter">
+            <li>Accueil</li>
+            <li>Articles
+            <li><span>watch dev is it?</span></li>
+            <li>Équipe</li>
+            <li>Contribuer</li>
+        </ul>
+        <div class="social">
+            <!-- Facebook-->
+            <a href="#"></a>
+            <!-- Instagram-->
+            <a href="#"></a>
+            <!-- Twitter-->
+            <a href="#"></a>
+            <!-- Linkedin-->
+            <a href="#"></a>
+        </div>
     </div>
 </footer>
 <script type="text/javascript" src="js/menu.js" id="menu-js"></script>
--- includes/_head.php
+++ includes/_head.php
@@ -1,3 +1,4 @@
+<meta name="robots" content="noindex">
 <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/header.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/article.css" media="screen">
