5923598820dac474c34cbd0fb0d376190cef2fcd
kelly <kelly.olida.pro@gmail.com>
2021-10-06 12:02:55 +0200

    added team php and css

--- team/index.php
+++ team/index.php
@@ -7,17 +7,55 @@
     <title>Watch Dev Is It?</title>
     <?php include("../includes/_head.php"); ?>
 </head>
+
 <body>
     <?php /* Start Header */ ?>
     <?php include("../includes/_header.php"); ?>
     <!-- Start Main Content -->
     <main>
-        <section class="welcome">
-            <h1>Welcome</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+
+        <h1>Notre équipe de Rédacteurs</h1>
+        <section class="redacteur">
+            <div >
+                <figure src="" alt="rédacteur, Kévin"></figure>
+                <h2>Kévin</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
                 tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#"><button>Start</button></a> 
+            </div>  
+            <div >
+                <figure src="" alt="rédacteur, Océane"></figure>
+                <h2>Océane</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>  
+            <div >
+                <figure src="" alt="rédacteur, Kelly"></figure>
+                <h2>Kelly</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>  
+            <div >
+                <figure src="" alt="rédacteur, Julien"></figure>
+                <h2>Julien</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>      
         </section>
+
+        <section class='contact-team'>
+                <div class="div-h2-paragraphe">
+                    <h2>Contactez-nous</h2>
+                    <p>Pour contacter notre équipe et nous partager vos artcles préferés c'est par ici.</p>
+                </div>
+
+                <div class="div-form">
+                    <form id="form-contact" name="form-contact">
+                        <input type="email" class="form-email" name="email" placeholder="Votre adresse email">
+                        <button>Soumettre</button>
+                    </form>
+                </div>
+        </section>
+
     </main>
     <!-- Start Footer -->
     <?php include("../includes/_footer.php"); ?>
