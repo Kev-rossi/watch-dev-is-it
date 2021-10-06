f196a18ddf6b7660213aed828a44bbffe041f6f1
oceansalle <oceanesalleiae@gmail.com>
2021-10-06 11:57:47 +0200

    ajout modification head and CSS

--- includes/_head.php
+++ includes/_head.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" type="text/css" href="css/header.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/article.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/team.css" media="screen">
