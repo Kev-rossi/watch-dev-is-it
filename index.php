a8243f2d2650045a764c239e31f0640859cf0ea2
Julien Roth <julien@infraroth.com>
2021-10-07 10:20:23 +0200

    Added new Blog home page

--- index.php
+++ index.php
@@ -4,23 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Watch Dev Is It?</title>
+    <title>Watch Dev Is It? | Un blog de rédacteurs développeurs</title>
     <?php include("includes/_head.php"); ?>
 </head>
-<body>
-    <?php /* Start Header */ ?>
+<body class="home">
     <?php include("includes/_header.php"); ?>
-    <!-- Start Main Content -->
     <main>
         <section class="welcome">
-            <h1>Welcome</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
-                tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#"><button>Start</button></a>
-        </section>
-        
+            <h1>Watch Dev Is It?</h1>
+            <p>Un blog de rédacteurs développeurs</p>
+        </section> 
         <section class="articles">
-            <article>
+            <article class="black">
+                <a href="/blog/internet-et-ses-effets-sur-notre-cerveau">
+                    <span>Sciences</span>
+                    <h2>Internet et ses effets sur notre cerveau</h2>
+                    <p>Internet est désormais bien ancré dans nos vies. Il suffit d’observer notre façon de communiquer, d’entretenir nos relations, et d’obtenir de l’information afin de s’en rendre compte. Mais il reste un aspect qui questionne encore les scientifiques : quels sont les effets d’Internet sur le cerveau humain ? Disons que nous pouvons observer des côtés positifs comme négatifs. En effet, l'accès à l’Internet possède de nombreux avantages : l’accès à l’information à tout moment, la communication devient plus simple et plus rapide, …</p>
+                    <button>Lire l'article</button>
+                </a>
+            </article>
+            <article class="gray">
                 <a href="#">
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
@@ -28,7 +31,7 @@
                     <button>Lire l'article</button>
                 </a>
             </article>
-            <article>
+            <article class="white">
                 <a href="#">
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
@@ -36,15 +39,7 @@
                     <button>Lire l'article</button>
                 </a>
             </article>
-            <article>
-                <a href="#">
-                    <span>Nom catégorie</span>
-                    <h2>Titre</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
-                </a>
-            </article>
-            <article>
+            <article class="white">
                 <a href="#">
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
@@ -54,7 +49,6 @@
             </article>
         </section>
     </main>
-    <!-- Start Footer -->
     <?php include("includes/_footer.php"); ?>
 </body>
 </html>     
