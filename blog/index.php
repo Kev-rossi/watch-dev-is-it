24bbed84d6f22ea2d3e56a32956a18b244dc373c
Julien Roth <julien@infraroth.com>
2021-10-07 13:34:15 +0200

    Updated style.css and removed button

--- blog/index.php
+++ blog/index.php
@@ -25,7 +25,7 @@
                     </figure>
                     <h2>Internet et ses effets sur notre cerveau</h2>
                     <p>Internet est désormais bien ancré dans nos vies. Il suffit d’observer notre façon de communiquer, d’entretenir nos relations, et d’obtenir de l’information afin de s’en rendre compte. Mais il reste un aspect qui questionne encore les scientifiques : quels sont les effets d’Internet sur le cerveau humain ? Disons que nous pouvons observer des côtés positifs comme négatifs. En effet, l'accès à l’Internet possède de nombreux avantages : l’accès à l’information à tout moment, la communication devient plus simple et plus rapide, …</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article>
@@ -36,7 +36,7 @@
                     </figure>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article>
@@ -47,7 +47,7 @@
                     </figure>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article>
@@ -58,7 +58,7 @@
                     </figure>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article>
@@ -69,7 +69,7 @@
                     </figure>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article>
@@ -80,7 +80,7 @@
                     </figure>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
         </section>
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                     <span>Sciences</span>
                     <h2>Internet et ses effets sur notre cerveau</h2>
                     <p>Internet est désormais bien ancré dans nos vies. Il suffit d’observer notre façon de communiquer, d’entretenir nos relations, et d’obtenir de l’information afin de s’en rendre compte. Mais il reste un aspect qui questionne encore les scientifiques : quels sont les effets d’Internet sur le cerveau humain ? Disons que nous pouvons observer des côtés positifs comme négatifs. En effet, l'accès à l’Internet possède de nombreux avantages : l’accès à l’information à tout moment, la communication devient plus simple et plus rapide, …</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article class="gray">
@@ -28,7 +28,7 @@
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article class="white">
@@ -36,7 +36,7 @@
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
             <article class="white">
@@ -44,7 +44,7 @@
                     <span>Nom catégorie</span>
                     <h2>Titre</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button>Lire l'article</button>
+                    <div class="button">Lire l'article</div>
                 </a>
             </article>
         </section>
